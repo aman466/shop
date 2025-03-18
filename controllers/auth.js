6ee540dd71dc088d779943430b8a7e6db91ea719
Aman Kumar <amankumar@Amans-MacBook-Air.local>
2025-03-19 00:35:51 +0530

    add Auth functionality

---
+++ controllers/auth.js
@@ -0,0 +1,85 @@
+const bcrypt = require("bcryptjs");
+
+const User = require("../models/user");
+
+exports.getLogin = (req, res, next) => {
+  console.log('login', req.flash("error")[0]);
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    errorMessage: req.flash("error")[0],
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  console.log('signup', req.flash("error")[0]);
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Signup",
+    errorMessage: req.flash("error")[0],
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  const { email, password } = req.body;
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        req.flash("error", "Invalid email or password");
+        return res.redirect("/login");
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then((doMatch) => {
+          if (!doMatch) {
+            req.flash("error", "Invalid email or password");
+            return res.redirect("/login");
+          }
+          req.session.isLoggedIn = true;
+          req.session.user = user;
+          req.session.save((err) => {
+            console.log(err);
+            return res.redirect("/");
+          });
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  User.findOne({ email: email })
+    .then((userDoc) => {
+      if (userDoc) {
+        req.flash("error", "Email already exists");
+        return res.redirect("/signup");
+      }
+      return bcrypt
+        .hash(password, 12)
+        .then((hashedPassword) => {
+          const user = new User({
+            email: email,
+            password: hashedPassword,
+            cart: { items: [] },
+          });
+          return user.save();
+        })
+        .then((result) => {
+          res.redirect("/login");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    console.log(err);
+    res.redirect("/");
+  });
+};
